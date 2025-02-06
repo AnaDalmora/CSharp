2f21312fb18ae798fdc32ec41ed1e9fddf1a1f00
Ana Julia Lopes <anajlsilveira@gmail.com>
2025-02-06 09:19:59 -0300

    Update estruturaRepeticao.cs

--- logicaProg/estruturaRepeticao.cs
+++ logicaProg/estruturaRepeticao.cs
@@ -37,52 +37,53 @@ public class estruturaRepeticao
             exercicio11();
             exercicio12();
             exercicio13();
-        }
-        switch (Resposta)
-        {
-            case 1:
-                exercicio1();
-                break;
-            case 2:
-                exercicio2();
-                break;
-            case 3:
-                exercicio3();
-                break;
-            case 4:
-                exercicio4();
-                break;
-            case 5:
-                exercicio5();
-                break;
-            case 6:
-                exercicio6();
-                break;
-            case 7:
-                exercicio7();
-                break;
-            case 8:
-                exercicio8();
-                break;
-            case 9:
-                exercicio9();
-                break;
-            case 10:
-                exercicio10();
-                break;
-            case 11:
-                exercicio11();
-                break;
-            case 12:
-                exercicio12();
-                break;
-            case 13:
-                exercicio13();
-                break;
-            default:
-                Console.WriteLine("Valor inválido");
-                break;
-
+        }else{
+            switch (Resposta)
+            {
+                case 1:
+                    exercicio1();
+                    break;
+                case 2:
+                    exercicio2();
+                    break;
+                case 3:
+                    exercicio3();
+                    break;
+                case 4:
+                    exercicio4();
+                    break;
+                case 5:
+                    exercicio5();
+                    break;
+                case 6:
+                    exercicio6();
+                    break;
+                case 7:
+                    exercicio7();
+                    break;
+                case 8:
+                    exercicio8();
+                    break;
+                case 9:
+                    exercicio9();
+                    break;
+                case 10:
+                    exercicio10();
+                    break;
+                case 11:
+                    exercicio11();
+                    break;
+                case 12:
+                    exercicio12();
+                    break;
+                case 13:
+                    exercicio13();
+                    break;
+                default:
+                    Console.WriteLine("Valor inválido");
+                    break;
+    
+            }
         }
     }
     private static void exercicio1()
