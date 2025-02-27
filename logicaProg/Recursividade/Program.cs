240c320576b436421dad4e34712764cc5f61a39d
Ana Julia Lopes <anajlsilveira@gmail.com>
2025-02-27 10:37:38 -0300

    Lista 08

---
+++ logicaProg/Recursividade/Program.cs
@@ -0,0 +1,107 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace Recursividade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            exercicio01();
+            exercicio02();
+            exercicio03();
+            exercicio04();
+            exercicio05();
+        }
+
+
+        static void exercicio01()
+        {
+            // Exercício 1: Potência
+            //Implemente uma função recursiva chamada Potencia que recebe dois parâmetros base e expoente e calcula a potência de base elevado a expoente.
+            try { 
+            int a = funcoes.AskInt("Insira um número");
+            int b = funcoes.AskInt("Insira a potência que você deseja elevar o numero");
+            Console.WriteLine($"{a}^{b} = {funcoes.Potencia(a,b)}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Insira apenas numeros inteiros");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro inesperado: {ex.Message}");
+            }
+
+        }
+        static void exercicio02()
+        {
+            //Exercício 2: Soma de Números Inteiros
+            //Crie uma função recursiva chamada Soma que recebe um número inteiro positivo n como parâmetro e retorna a soma de todos os números inteiros de 1 até n.
+            try { 
+            int n = funcoes.AskInt("Insira um número inteiro");
+            Console.WriteLine($"A soma dos numeros entre 1 e {n} é {funcoes.Soma(n)}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Insira apenas numeros inteiros");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Erro inesperado: {ex.Message}");
+            }
+        }
+        static void exercicio03()
+        {
+            // Exercício 3: Fibonacci
+            //Desenvolva uma função recursiva chamada Fibonacci que recebe um número inteiro n como parâmetro e retorna o n - ésimo termo da sequência de Fibonacci.
+            try
+            {
+                Console.WriteLine("Fibonacci de 5 termos:\n");
+                funcoes.Fibonacci(0, 1, 5);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro inesperado: {ex.Message}");
+            }
+        }
+        static void exercicio04()
+        {
+            //Exercício 4: Contagem Regressiva
+            //Escreva uma função recursiva chamada ContagemRegressiva que recebe um número inteiro positivo n como parâmetro e exibe uma
+            //contagem regressiva de n até 1.
+            try{ 
+            int n = funcoes.AskInt("Insira um número para executar a contagem regressiva");
+            funcoes.ContagemRegressiva(n);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Insira apenas numeros inteiros");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro inesperado: {ex.Message}");
+            }
+        }
+        static void exercicio05()
+        {
+            //Exercício 5: Máximo Divisor Comum (MDC)
+            //Crie uma função recursiva chamada MDC que recebe dois números inteiros positivos a e b
+            //como parâmetros e retorna o máximo divisor comum entre eles.
+            try
+            {
+                int a = funcoes.AskInt("Insira um A");
+                int b = funcoes.AskInt("Insira um B");
+                funcoes.MDC(a, b);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Insira apenas numeros inteiros");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Erro inesperado: {ex.Message}");
+            }
+        
+        }
+    }
+}
---
+++ logicaProg/Recursividade/funcoes.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Recursividade
+{
+    internal class funcoes
+    {
+       public static int AskInt(string pergunta)
+        {
+            Console.WriteLine(pergunta);
+            return int.Parse(Console.ReadLine());
+        }
+        public static int Potencia(int a, int b)
+        {
+            if (b == 1) return a * 1;
+            return a * Potencia(a, b - 1);
+        }
+        public static double Soma(int n)
+        {
+            if (n == 0) return 0;
+            return n + Soma(n - 1);
+        }
+        public static void ContagemRegressiva(int n)
+        {
+            if (n == 1)
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                Console.WriteLine(n);
+                ContagemRegressiva(n - 1);
+            }
+        }
+        public static void Fibonacci(int termo1,int termo2, int n)
+        {
+            if (n !=0 )
+            {
+                Console.WriteLine(termo1);
+                Fibonacci(termo2, termo1 + termo2, n - 1);
+            }
+        }
+
+        public static void MDC(int a, int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("O MDC é "+a);
+            }
+            else 
+            {
+                MDC(b, a % b);
+            }
+        }
+
+    }
+}
