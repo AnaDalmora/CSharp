2247bf1f4af2e3ab99e50e61fcba220dd5c67955
Ana Julia Lopes <anajlsilveira@gmail.com>
2025-03-06 11:20:46 -0300

    Primeiro Exercicio

---
+++ POO/SistemaPedidosLanchonete/Pedido.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaPedidosLanchonete
+{
+    public class Pedido
+    {
+        public int NumeroPedido;
+        public List<Produto> itens = new List<Produto> ();
+
+        public void ExibirPedido()
+        {
+            Console.WriteLine($"\nPedido #{NumeroPedido}");
+            Console.WriteLine("Itens:");
+            foreach(var item in itens)
+            {
+                Console.WriteLine($"- {item.NomeProduto} - R$ {item.Preco}");
+            }
+            Console.WriteLine($"Total: R$ {CalcularValorTotalPedido()}");
+        }
+        public float CalcularValorTotalPedido()
+        {
+            float totalPedido = 0;
+            foreach(var item in itens)
+            {
+                totalPedido += item.Preco;
+            }
+            return totalPedido;
+        }
+        
+    }
+}
---
+++ POO/SistemaPedidosLanchonete/Produto.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace SistemaPedidosLanchonete
+{
+    public class Produto
+    {
+        public string NomeProduto;
+        public float Preco;
+    }
+}
