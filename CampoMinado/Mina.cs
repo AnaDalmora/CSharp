01f5a16e301b1bfe4fdba1902b1f071e5dbed5da
Ana Julia Dalmora <anajlsilveira@gmail.com>
2025-04-14 09:25:18 -0300

    Update Mina.cs

--- CampoMinado/Mina.cs
+++ CampoMinado/Mina.cs
@@ -4,7 +4,7 @@
 
     public int GerarMina()
     {
-        // 80% de aparecer 0 e 15% de aparecer 1. 0 = sem mina, 1 = mina
+        // 75% de aparecer 0 e 15% de aparecer 1. 0 = sem mina, 1 = mina
         return random.NextDouble() < 0.15 ? 1 : 0;
     }
 }
